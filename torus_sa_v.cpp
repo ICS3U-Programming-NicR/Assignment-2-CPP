54cb86e5caa6241d5dd0268c0f0e7f4e806c010a
nictheriscer <nicolas.riscalas@stu.ocsb.ca>
2022-03-31 21:57:09 +0000

    dfs

--- torus_sa_v.cpp
+++ torus_sa_v.cpp
@@ -14,25 +14,33 @@ using std::string;
 
 int main() {
     float majRadius, minRadius, surfaceArea, volume;
-    cout << "Enter the Major Radius (the distance from the center of the tube to the center of the torus) ";
+    cout << "Enter the Major Radius (the distance from "\
+    "the center of the tube to the center of the torus) ";
     cin >> majRadius;
     cout << "Enter the Minor Radius (the radius of the tube) ";
     cin >> minRadius;
-    if (majRadius > minRadius){
+    if (majRadius > minRadius) {
         cout << "This torus is a ring torus\n";
         surfaceArea = 4 * pow(M_PI, 2) * minRadius * majRadius;
         volume = 2 * pow(M_PI, 2) * pow(minRadius, 2) * majRadius;
-        cout << "The volume is "<< volume <<" and the surface area is "<< surfaceArea<< "\n";
-    }else if (majRadius == minRadius){
+        cout << "The volume is "<< volume \
+        <<" and the surface area is "<< surfaceArea<< "\n";
+    } else if (majRadius == minRadius) {
         cout << "This torus is a horn torus\n";
         surfaceArea = 4 * pow(M_PI, 2) * minRadius * majRadius;
         volume = 2 * pow(M_PI, 2) * pow(minRadius, 2) * majRadius;
-        cout << "The volume is "<< volume <<" and the surface area is "<< surfaceArea<< "\n";
-    }else if (majRadius < minRadius){
+        cout << "The volume is "<< volume \
+        <<" and the surface area is "<< surfaceArea<< "\n";
+    } else if (majRadius < minRadius) {
         cout << "This torus is a spindle torus\n";
         surfaceArea = 4 * pow(M_PI, 2) * pow(minRadius, 2);
-        volume = (2 / 3) * M_PI * (2 * pow(minRadius, 2) + pow(majRadius, 2)) * sqrt(pow(minRadius, 2) - pow(majRadius, 2)) + M_PI * pow(minRadius, 2) * majRadius * ( M_PI + 2 * atan(majRadius / sqrt(pow(minRadius, 2) - pow(majRadius, 2))));
-        cout << "The volume is "<< volume <<" and the surface area is "<< surfaceArea<< "\n";
+        volume = (2 / 3) * M_PI * (2 * pow(minRadius, 2) \
+        + pow(majRadius, 2)) * sqrt(pow(minRadius, 2) - \
+        pow(majRadius, 2)) + M_PI * pow(minRadius, 2) * \
+            majRadius * (M_PI + 2 * atan(majRadius / \
+            sqrt(pow(minRadius, 2) - pow(majRadius, 2))));
+        cout << "The volume is "<< volume \
+        <<" and the surface area is "<< surfaceArea<< "\n";
     }
     string tryAgain;
     cout << "would you like to try again? ";
